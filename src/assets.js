68b2e958a6d0c89b3c827be29f7a612dab7fa271
Laisses <laisse@gmail.com>
2022-09-29 17:24:06 -0300

    add questions

--- src/assets.js
+++ src/assets.js
@@ -9,5 +9,46 @@ import seta_virar from "./assets/seta_virar.png";
 export const images = {
     icone_certo, icone_erro, icone_quase, logo, sad, seta_play, seta_virar,
 };
-    
 
+export const questions = [
+    {
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript",
+        category: "Módulo React",
+    },
+    {
+        question: "O React é __",
+        answer: "uma biblioteca JavaScript para construção de interfaces",
+        category: "Módulo React",
+    },
+    {
+        question: "Componentes devem iniciar com __",
+        answer: "letra maiúscula",
+        category: "Módulo React",
+    },
+    {
+        question: "Podemos colocar __ dentro do JSX",
+        answer: "expressões",
+        category: "Módulo React",
+    },
+    {
+        question: "O ReactDOM nos ajuda __",
+        answer: "interagindo com a DOM para colocar componentes React na mesma",
+        category: "Módulo React",
+    },
+    {
+        question: "Usamos o npm para __",
+        answer: "gerenciar os pacotes necessários e suas dependências",
+        category: "Módulo React",
+    },
+    {
+        question: "Usamos props para __ ",
+        answer: "passar diferentes informações para componentes ",
+        category: "Módulo React",
+    },
+    {
+        question: "Usamos estado (state) para __",
+        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+        category: "Módulo React",
+    }
+];
