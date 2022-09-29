510025e9c040dfbb677ecf27aa683286259b992c
Laisses <laisse@gmail.com>
2022-09-29 10:55:11 -0300

    import components to App file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { GlobalStyle } from "./GlobalStyle";
+import styled from "styled-components";
+import { Navbar } from "./Navbar";
+import { Questions } from "./Questions";
+import { UserAnswer } from "./Footer";
 
 export const App = () => {
     return (
-        <>
+        <Screen>
             <GlobalStyle />
-            <h1>Oie222</h1>
-        </>
+            <Navbar />
+            <Questions />
+            <UserAnswer />
+        </Screen>
     );
 };
+
+const Screen = styled.div`
+    background-color: #FB6B6B;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    padding-bottom: 200px;
+`;
+//exportar o setStuff para o filho, para ele ter poder de alterar o estado no pai
