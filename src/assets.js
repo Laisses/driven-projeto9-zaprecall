d48b88ce4e4c4a4cde90d808ca5548e461fa8485
Laisses <laisse@gmail.com>
2022-09-29 10:53:25 -0300

    create assets folder and import images to it

---
+++ src/assets.js
@@ -0,0 +1,13 @@
+import icone_certo from "./assets/icone_certo.png";
+import icone_erro from "./assets/icone_erro.png";
+import icone_quase from "./assets/icone_quase.png";
+import logo from "./assets/logo.png";
+import sad from "./assets/sad.png";
+import seta_play from "./assets/seta_play.png";
+import seta_virar from "./assets/seta_virar.png";
+
+export const images = {
+    icone_certo, icone_erro, icone_quase, logo, sad, seta_play, seta_virar,
+};
+    
+
